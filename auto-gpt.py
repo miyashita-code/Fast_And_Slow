9d1a6dc1ee6f2436ec5754de5b2fad0b408f6bb6
Takuma Miyashita <miyashita.tech@gmail.com>
2023-12-29 03:13:02 +0900

    add chat interface with streamlit and langchain for demo of fast and slow concept

---
+++ auto-gpt.py
@@ -0,0 +1,207 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from langchain.llms import OpenAI
+from langchain.chains.llm import LLMChain
+from langchain.chat_models.base import BaseChatModel
+from langchain.memory import ChatMessageHistory
+from langchain.schema import (
+    BaseChatMessageHistory,
+    Document,
+)
+from langchain.schema.messages import AIMessage, HumanMessage, SystemMessage
+from langchain.schema.vectorstore import VectorStoreRetriever
+from langchain.tools.base import BaseTool
+from langchain.tools.human.tool import HumanInputRun
+from langchain.agents import load_tools
+
+from langchain_experimental.autonomous_agents.autogpt.output_parser import (
+    AutoGPTOutputParser,
+    BaseAutoGPTOutputParser,
+)
+from langchain_experimental.autonomous_agents.autogpt.prompt import AutoGPTPrompt
+from langchain_experimental.autonomous_agents.autogpt.prompt_generator import (
+    FINISH_NAME,
+)
+from langchain_experimental.pydantic_v1 import ValidationError
+
+# Lang chain側のインポート
+from datetime import datetime
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.memory import VectorStoreRetrieverMemory
+from langchain.chains import ConversationChain
+from langchain.prompts import PromptTemplate
+
+
+import faiss
+
+from langchain.docstore import InMemoryDocstore
+from langchain.vectorstores import FAISS
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+class AutoGPT:
+    """Agent class for interacting with Auto-GPT."""
+
+    def __init__(
+        self,
+        ai_name: str,
+        memory: VectorStoreRetriever,
+        chain: LLMChain,
+        output_parser: BaseAutoGPTOutputParser,
+        tools: List[BaseTool],
+        feedback_tool: Optional[HumanInputRun] = None,
+        chat_history_memory: Optional[BaseChatMessageHistory] = None,
+    ):
+        self.ai_name = ai_name
+        self.memory = memory
+        self.next_action_count = 0
+        self.chain = chain
+        self.output_parser = output_parser
+        self.tools = tools
+        self.feedback_tool = feedback_tool
+        self.chat_history_memory = chat_history_memory or ChatMessageHistory()
+
+
+    @classmethod
+    def from_llm_and_tools(
+        cls,
+        ai_name: str,
+        ai_role: str,
+        memory: VectorStoreRetriever,
+        tools: List[BaseTool],
+        llm: BaseChatModel,
+        human_in_the_loop: bool = False,
+        output_parser: Optional[BaseAutoGPTOutputParser] = None,
+        chat_history_memory: Optional[BaseChatMessageHistory] = None,
+    ) -> AutoGPT:
+        prompt = AutoGPTPrompt(
+            ai_name=ai_name,
+            ai_role=ai_role,
+            tools=tools,
+            input_variables=["memory", "messages", "goals", "user_input"],
+            token_counter=llm.get_num_tokens,
+        )
+        human_feedback_tool = HumanInputRun() if human_in_the_loop else None
+        chain = LLMChain(llm=llm, prompt=prompt)
+        return cls(
+            ai_name,
+            memory,
+            chain,
+            output_parser or AutoGPTOutputParser(),
+            tools,
+            feedback_tool=human_feedback_tool,
+            chat_history_memory=chat_history_memory,
+        )
+
+
+    def run(self, goals: List[str]) -> str:
+        user_input = (
+            "Determine which next command to use, "
+            "and respond using the format specified above:"
+        )
+        # Interaction Loop
+        loop_count = 0
+        while True:
+            # Discontinue if continuous limit is reached
+            loop_count += 1
+
+            # Send message to AI, get response
+            assistant_reply = self.chain.run(
+                goals=goals,
+                messages=self.chat_history_memory.messages,
+                memory=self.memory,
+                user_input=user_input,
+            )
+
+            # Print Assistant thoughts
+            print(assistant_reply)
+            self.chat_history_memory.add_message(HumanMessage(content=user_input))
+            self.chat_history_memory.add_message(AIMessage(content=assistant_reply))
+
+            # Get command name and arguments
+            action = self.output_parser.parse(assistant_reply)
+            tools = {t.name: t for t in self.tools}
+            if action.name == FINISH_NAME:
+                return action.args["response"]
+            if action.name in tools:
+                tool = tools[action.name]
+                try:
+                    observation = tool.run(action.args)
+                except ValidationError as e:
+                    observation = (
+                        f"Validation Error in args: {str(e)}, args: {action.args}"
+                    )
+                except Exception as e:
+                    observation = (
+                        f"Error: {str(e)}, {type(e).__name__}, args: {action.args}"
+                    )
+                result = f"Command {tool.name} returned: {observation}"
+            elif action.name == "ERROR":
+                result = f"Error: {action.args}. "
+            else:
+                result = (
+                    f"Unknown command '{action.name}'. "
+                    f"Please refer to the 'COMMANDS' list for available "
+                    f"commands and only respond in the specified JSON format."
+                )
+
+            memory_to_add = (
+                f"Assistant Reply: {assistant_reply} " f"\nResult: {result} "
+            )
+            if self.feedback_tool is not None:
+                feedback = f"\n{self.feedback_tool.run('Input: ')}"
+                if feedback in {"q", "stop"}:
+                    print("EXITING")
+                    return "EXITING"
+                memory_to_add += feedback
+
+            self.memory.add_documents([Document(page_content=memory_to_add)])
+            self.chat_history_memory.add_message(SystemMessage(content=result))
+
+def main():
+
+    # Define your embedding model
+    embeddings_model = OpenAIEmbeddings()
+
+    # Initialize the vectorstore as empty
+    embedding_size = 1536
+    # L2ノルム全探索用
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+
+    retriever = vectorstore.as_retriever(search_kwargs=dict(k=5))
+    memory = vectorstore.as_retriever()
+
+
+    # init llm
+    llm = OpenAI(temperature=0)
+
+    # load google search tool
+    tools = load_tools(["serpapi"], llm=llm)
+
+    auto_gpt = AutoGPT.from_llm_and_tools(
+        ai_name="介助者",
+        ai_role="認知症患者の生活における意思決定支援や不安解消を行う情緒的なケアギバー",
+        memory=memory,
+        tools=tools,
+        llm=llm,
+    )
+
+    # Set verbose to be true
+    auto_gpt.chain.verbose = True
+
+    auto_gpt.run(goals=["これなんか閉まらないに対応する"])
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ chat_app.py
@@ -0,0 +1,112 @@
+import sys
+
+import os
+import openai
+import streamlit as st
+from dotenv import load_dotenv
+
+from modules.fast_agent import Fast_Agents
+
+
+
+def init():
+    # Load the OpenAI API key from the environment variable
+    load_dotenv()
+    
+    # test that the API key exists
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        #print("OPENAI_API_KEY is set")
+        pass
+
+    # setup streamlit page
+    st.set_page_config(
+        page_title="Fast & Slow"
+    )
+
+    if "instructions" not in st.session_state:
+        st.session_state.instructions = ""
+
+    if "prev_option" not in st.session_state:
+        st.session_state.prev_option = "gpt4"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    if "agents" not in st.session_state:
+        st.session_state.agents = Fast_Agents(st.session_state.instructions)
+
+        # update chat history
+        st.session_state.messages = st.session_state.agents.parse_chat_history_into_streamlit_chat_format()
+
+
+
+
+    
+    
+def main():
+    with st.sidebar:
+        st.title("Fast & Slow💬")
+        
+        # model selection
+        if option := st.selectbox(
+                "Choose Tryal Agent",
+                ("fast_and_slow", "gpt4_CoT", "gpt4"),
+                index=2,
+                placeholder="Select contact method...",
+            ):
+
+            if st.session_state.prev_option != option:
+                st.session_state.prev_option = option
+
+                # change current chain
+                st.session_state.agents.set_chain(option)
+
+                # update chat history
+                st.session_state.messages = st.session_state.agents.parse_chat_history_into_streamlit_chat_format()
+
+        # show instruction 
+        st.subheader("Instructions")
+        st.markdown(st.session_state.instructions if st.session_state.instructions != "" else "None")
+
+
+
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Agentにメッセージを送る"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            input = {"input": st.session_state.messages[-1]["content"]}  
+            contents = {"input": input["input"], "instruction": st.session_state.instructions} if (st.session_state.agents.current_chain_with_mem_name == "fast_and_slow") else {"input": input["input"]}
+                      
+            with st.spinner(""):
+                # Stream
+                for s in st.session_state.agents.current_chain_with_mem["data"]["chain"].stream(contents):        
+                    full_response += s
+                    message_placeholder.markdown(full_response)
+
+            #message_placeholder.markdown(full_response)
+            st.session_state.agents.current_chain_with_mem["data"]["memory"].save_context(input, {"output": full_response})
+
+    
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+if __name__ == "__main__":
+    init()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
