29fdf6a9238c78a6cf397b9547e4c74b6ab7ee68
unknown <miyashita.tech@gmail.com>
2024-04-19 17:50:27 +0900

    fix wrong usage of send_socket in bgprocess run.

--- modules/backend_process.py
+++ modules/backend_process.py
@@ -41,11 +41,9 @@ class BackEndProcess:
         print(f"run : {self.room}")
         print(f"say hello : {self.room}")
         self.socketio.emit('announce', {'announce': 'Hello, autoGpt Started!'}, room=self.room)
-
-        self.send_socket("instruction", "まずは、傾聴を心がけてください。ユーザーの状態を把握することが第一の目標です。虚偽の事実を伝えないように十分に注意してください。")
+        self.send_socket("instruction", {"instruction" : "まずは、傾聴を心がけてください。ユーザーの状態を把握することが第一の目標です。虚偽の事実を伝えないように十分に注意してください。", "isLendingEar" : True})
         self.isFin = False
         autogpt_main(self.send_socket, self.get_messages, self.isFin)
-
 
 
     def send_socket(self, event, data):
