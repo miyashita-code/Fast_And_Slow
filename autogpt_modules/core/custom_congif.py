4a2fdf1f6c21b00b13200d63c3fa501cdfebf697
unknown <miyashita.tech@gmail.com>
2024-04-21 05:48:16 +0900

    use open ai gpt-4turbo

--- autogpt_modules/core/custom_congif.py
+++ autogpt_modules/core/custom_congif.py
@@ -1,8 +1,8 @@
-#MODEL = "gpt-4-0125-preview"
+MODEL = "gpt-4-turbo"
 #MODEL="gpt-3.5-turbo-1106"
-#MODEL_TYPE = "openai"
-MODEL_TYPE = "anthropic"
-MODEL = "claude-3-haiku-20240307"
+MODEL_TYPE = "openai"
+#MODEL_TYPE = "anthropic"
+#MODEL = "claude-3-haiku-20240307"
 THRED_CONSEQ_WAIT = 30
 MAX_TOKEN_WINDOW = 10000
 
